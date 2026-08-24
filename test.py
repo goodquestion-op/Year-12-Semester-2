5c84424cb3016bf3218e5dd66a2da33f749b88f1
goodquestion-op <0348422@schoolsnet.act.edu.au>
2026-08-24 10:12:00 +1000

    yes?

--- test.py
+++ test.py
@@ -1,2 +1,26 @@
-import opencv
+from PIL import Image
+import pytesseract
+import cv2
+import os, sys, inspect #For dynamic filepaths
+import numpy as np;
+
+#Find the execution path and join it with the direct reference
+def execution_path(filename):
+  return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), filename)	 
+import pytesseract
+import cv2
+import os, sys, inspect #For dynamic filepaths
+import numpy as np;
+
+#Find the execution path and join it with the direct reference
+def execution_path(filename):
+  return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), filename)	
+
+
+filename = execution_path("1_python-ocr.jpg")
+img = np.array(Image.open(filename))
+cv2.imshow('image', img)
+text = pytesseract.image_to_string(img)
+
+print(text)
 
