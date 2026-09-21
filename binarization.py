c6a22c3e4c37247f4986687cb873a75c827bfa64
goodquestion <ashwynswainson@gmail.com>
2026-09-21 10:26:51 +1000

    adding more bounding oxes

--- binarization.py
+++ binarization.py
@@ -35,6 +35,7 @@ while True:
     greyscale = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
     img3 = cv2.adaptiveThreshold(greyscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 5)
     dilate = cv2.erode(img3, kernel, iterations = 1)
+    color = cv2.cvtColor(dilate, cv2.COLOR_GRAY2BGR) #to debug better
     
    # doesn't seem to help img4 = cv2.GaussianBlur(img3, (1, 1), 0)
 
@@ -72,43 +73,50 @@ while True:
 
     for b in dilateB.splitlines():
             b = b.split(' ')
-            print(b)
+        #     print(b)
             x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-            cv2.rectangle(dilate, (x, hImg - y), (w, hImg - h), (50,50,255),1)
-            cv2.putText(dilate, b[0], (x,hImg - y + 13), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (50,205,50), 1)
-            if bestX < x:
+                                #  top left       bottom right 
+            cv2.rectangle(color, (x, hImg - y), (w, hImg - h), (50,50,255),1)
+            
+            cv2.putText(color, b[0], (x,hImg - y + 13), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (50,205,50), 1)
+            if bestX > x:
                   bestX = x
-                  print(bestX)
-            if bestY < y:
-                    bestY = y
-                    print(bestY)
+                #   print(bestX)
+            if bestY < (hImg - y):
+                    bestY = (hImg - y)
+                    # print(bestY)
             if bestW < w:
                     bestW = w
-                    print(bestW)
-            if bestH < h:
-                    bestH = h
-                    print(bestH)
-    cv2.rectangle(dilate, (bestX, hImg - bestY), (bestW, hImg - bestH), (50,50,255),1)
+                    # print(bestW)
+            if bestH > (hImg - h):
+                    bestH = (hImg - h)
+                    # print(bestH)
+    cv2.rectangle(color, (bestX, bestY), (bestW, bestH), (255,150,0),2)
 
     
     text4 = pytesseract.image_to_boxes(img3)
     text = pytesseract.image_to_boxes(img)
-    #debug = pytesseract.image_to_data(dilate)
-  
+    debug = pytesseract.image_to_boxes(dilate)
+
+    
+
+    #debuging 
+    #cv2.rectangle(color, (-5, hImg - 100), (315, hImg - 80), (0,255,255),2)
 
 
 
     # Output
 
-    cv2.imshow("Original", img)
-    cv2.imshow("Thresh", img3)
-    cv2.imshow("grey", greyscale)
-    cv2.imshow("dilate", dilate)
-    
+    # cv2.imshow("Original", img)
+    # cv2.imshow("Thresh", img3)
+    # cv2.imshow("grey", greyscale)
+    # cv2.imshow("dilate", dilate)
+    cv2.imshow("color2", color)
+
 
     #print("Thresh:"+text4)
     #print("org:"+text)
-    #print("debug:"+debug)
+    print("debug:"+debug)
     
 
 
